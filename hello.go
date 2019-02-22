9540d5c636f02873700b7d0921ddd3e1c40291dc
阿部直也 <n.abe@gemcook.com>
2019-02-22 20:24:53 +0900

    first-go

---
+++ hello.go
@@ -0,0 +1,19 @@
+// hello.go
+package main
+
+import "fmt"
+
+/*
+func init() {
+	fmt.Println("init")
+}
+*/
+
+func bazz() {
+	fmt.Println("bazz")
+}
+
+func main() {
+	// bazz()
+	fmt.Println("Hello world!!", "TEST TEST")
+}
---
+++ lesson7.go
@@ -0,0 +1,2 @@
+// lesson7.go
+package main
