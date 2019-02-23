859f749a97e93109090ff9642aa7077e0388db4f
阿部直也 <n.abe@gemcook.com>
2019-02-23 16:24:50 +0900

    [update] 2/23

--- hello.go
+++ hello.go
@@ -1,6 +1,8 @@
 // hello.go
 package main
 
+import "fmt"
+
 func main() {
 	// var x int = 1
 	// xx := float64(x)
@@ -44,4 +46,52 @@ func main() {
 
 	// n = append(n, 100, 200, 300, 400)
 	// fmt.Println(n)
+
+	// n := make([]int, 3, 5)
+	// fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
+	// n = append(n, 0, 0)
+	// fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
+	// n = append(n, 1)
+	// fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
+
+	// a := make([]int, 3)
+	// fmt.Printf("len=%d cap=%d value=%v\n", len(a), cap(a), a)
+
+	// b := make([]int, 0)
+	// var c []int
+	// fmt.Printf("len=%d cap=%d value=%v\n", len(b), cap(b), b)
+	// fmt.Printf("len=%d cap=%d value=%v\n", len(c), cap(c), c)
+
+	// c = make([]int, 5)
+	// // c = make([]int, 0, 5)
+	// for i := 0; i < 5; i++ {
+	// 	c = append(c, i)
+	// 	fmt.Println(c)
+	// }
+	// fmt.Println(c)
+
+	m := map[string]int{"apple": 100, "banana": 200}
+	fmt.Println(m)
+	fmt.Println(m["apple"])
+	m["banana"] = 300
+	fmt.Println(m)
+	m["peach"] = 500
+	fmt.Println(m)
+
+	fmt.Println(m["noting"])
+
+	v, ok := m["apple"]
+	fmt.Println(v, ok)
+
+	v2, ok2 := m["orange"]
+	fmt.Println(v2, ok2)
+
+	m2 := make(map[string]int)
+	m2["pc"] = 5000
+	fmt.Println(m2)
+
+	var s []int
+	if s == nil {
+		fmt.Println("nil")
+	}
 }
